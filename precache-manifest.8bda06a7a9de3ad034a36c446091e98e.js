6cd9cdaeb7797166e8e27df777b5d458603b4fd3
Irfan Ansari <exitir@gmail.com>
2020-07-02 11:58:52 +0530

    Updates

---
+++ precache-manifest.8bda06a7a9de3ad034a36c446091e98e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4895c0323f701fd2ddbad8f5cb1e6c1b",
+    "url": "/irfan-robo-friends/index.html"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/irfan-robo-friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "315f7ec4041ba1556f7c",
+    "url": "/irfan-robo-friends/static/css/main.4dcb3451.chunk.css"
+  },
+  {
+    "revision": "cc0d58ead9737cd69c0e",
+    "url": "/irfan-robo-friends/static/js/2.2a06c861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/irfan-robo-friends/static/js/2.2a06c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "315f7ec4041ba1556f7c",
+    "url": "/irfan-robo-friends/static/js/main.bf7637b7.chunk.js"
+  },
+  {
+    "revision": "10cd7d608a46e2e713b8",
+    "url": "/irfan-robo-friends/static/js/runtime-main.5bd9982e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/irfan-robo-friends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
