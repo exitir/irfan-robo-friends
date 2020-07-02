3d391dcaf87064ad5ec3e35bf6698fe935f8e1f3
Irfan Ansari <exitir@gmail.com>
2020-07-02 17:13:08 +0530

    reverting url change

--- src/containers/App.js
+++ src/containers/App.js
@@ -24,8 +24,7 @@ class App extends React.Component {
         };
     }
     componentDidMount() {
-        // fetch('https://jsonplaceholder.typicode.com/users')
-        fetch('http://my-json-server.typicode.com/exitir/irfan-json-placeholder/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => this.setState({ robots: users }));
 
